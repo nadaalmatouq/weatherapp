cb15ba1e786c9cae3ac53ca8c91f4137a83cd1ec
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-03 01:47:34 +0300

    test

---
+++ weatherapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weatherapp
+//
+//  Created by Nada Alm on 5/3/20.
+//  Copyright © 2020 Nada Alm. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
