8fd3b38a6260ac61cd24dd0c4bf92e590fafef95
nada <64605593+nadaalmatouq@users.noreply.github.com>
2020-05-03 04:19:02 +0300

    spacer problem unresolved

--- weatherapp/ContentView.swift
+++ weatherapp/ContentView.swift
@@ -10,9 +10,87 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack{
+            
+            Image("Night").edgesIgnoringSafeArea(.all)
+           
+            VStack{
+            
+            VStack{
+                
+                Text("Mishrif").foregroundColor(.white).font(.largeTitle).bold().multilineTextAlignment(.center)
+                
+                Text("Mostly clear").foregroundColor(.white)
+                
+                Text("22°").foregroundColor(.white).font(.system(size: 90)).multilineTextAlignment(.center).padding(.horizontal).offset(x: 10)
+                
+                
+                
+            }.offset(y: -150)
+            
+            HStack{
+                Text("Tuesday").foregroundColor(.white).bold()
+                
+                Text("TODAY").foregroundColor(.white)
+           
+            Spacer()
+                
+                Text("31").foregroundColor(.white)
+                Text("24").foregroundColor(.white)
+                
+            }
+         
+                HStack(alignment: .center,spacing: 10){
+             
+                
+                Text("Now").foregroundColor(.white).bold()
+                Text("5AM").foregroundColor(.white).bold()
+                Text("6AM").foregroundColor(.white).bold()
+                Text("7AM").foregroundColor(.white).bold()
+                Text("8AM").foregroundColor(.white).bold()
+                Text("9AM").foregroundColor(.white).bold()
+                Text("10AM").foregroundColor(.white).bold()
+                    
+            }
+                HStack(alignment: .center,spacing: 20){
+                 
+                    
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    Text("22°").foregroundColor(.white).bold()
+                    
+                    
+                }
+                
+                HStack{
+                    
+                    
+                    Text("Wednesday").foregroundColor(.white).bold()
+                    
+                     Text("29").foregroundColor(.white).bold()
+                     Text("24").foregroundColor(.white).bold()
+                    
+                    
+                    
+                }
+                
+            
+            
+            
+            
+        }
+        
+        }
+        
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
